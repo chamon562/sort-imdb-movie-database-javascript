526a95a176ef1c2d79ab3e2a23cd24cfe0b1b43e
unknown <Chamon562@gmail.com>
2021-03-03 15:57:25 -0800

    initial setup test commit after putting in HTML boilerplaste and some psuedo code in app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+/**
+ * TODO: Change sortMoviesByRank() function to sort movies list by rank
+ * TODO: Sort movies by id, rank, and title through dynamic function
+ * TODO: Create helper function called getMaxMovieObject() for finding max movie
+ */
